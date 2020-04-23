8d80a107771d52beecc6f08902a83a13d4c43e19
Goutham-AR <argouth8400@gmail.com>
2020-04-23 19:19:11 +0530

    Completed

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const methodOverride = require("method-override");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -5,11 +6,13 @@ const bodyParser = require("body-parser");
 
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useUnifiedTopology", true);
+mongoose.set('useFindAndModify', false);
 mongoose.connect("mongodb://localhost/blogDB");
 //APP/Config....
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 // Mongoose/Config....
 /* 
@@ -56,6 +59,50 @@ app.get("/blogs/new", function(req, res) {
 // CREATE ROUTE
 app.post("/blogs", function(req, res) {
     blog.create(req.body.blog, function(err, newBlog) {
+        if(err) {
+            console.log("error", err);
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+// SHOW ROUTE
+app.get("/blogs/:id", function(req, res) {
+    blog.findById(req.params.id, function(err, foundBlog) {
+        if(err) {
+            console.log("error", err);
+        } else {
+            res.render("show", { blogPost: foundBlog });
+        }
+    });
+});
+
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res) {
+    blog.findById(req.params.id, function(err, foundBlog) {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function(req, res) {
+    blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
+// DELETE ROUTE
+app.delete("/blogs/:id", function(req, res) {
+    blog.findByIdAndRemove(req.params.id, function(err) {
         if(err) {
             console.log("error", err);
         } else {
